999cb5ed96c0f5e0f82301e59c8fa3b6bc3a969f
Alex Pishvanov <apishvanov@gmail.com>
2024-02-07 23:15:57 +0200

    removed artifacts

--- hw1/src/main/java/ru/otus/spring/hw1/Application.java
+++ hw1/src/main/java/ru/otus/spring/hw1/Application.java
@@ -1,14 +1,11 @@
 package ru.otus.spring.hw1;
 
-import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.spring.hw1.service.TestRunnerService;
 
-import java.io.IOException;
-
 public class Application {
-    public static void main(String[] args) throws CsvValidationException, IOException {
+    public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
         var testRunnerService = context.getBean(TestRunnerService.class);
